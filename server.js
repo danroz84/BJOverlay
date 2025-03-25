9f1452d205d97429f84300b27b0f67142edde8ba
danroz84 <rozo.daniel84@gmail.com>
2025-03-25 15:41:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const WebSocket = require('ws');
 const express = require('express');
 const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
-const server = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Servidor corriendo en ${PORT}`));
 
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Iniciar servidor HTTP
+const server = app.listen(PORT, () => console.log(`Servidor corriendo en ${PORT}`));
+
+// Configurar WebSockets
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
     console.log('Nuevo cliente conectado');
-    
+
     ws.on('message', (message) => {
         console.log(`Mensaje recibido: ${message}`);
         wss.clients.forEach(client => {
